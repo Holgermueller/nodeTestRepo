51db01c71e67a649679d5a6a065cda7f57be7c18
holgermueller <9jholger@gmail.com>
2023-11-18 11:42:45 -0600

    removed consolelogs

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ const button = document.getElementById("modalButton");
 const closeButton = document.getElementById("span");
 
 const showModal = () => {
-  console.log("bing");
-
   modal.style.display = "block";
 };
 
 const hideModal = () => {
-  console.log("click");
-
   modal.style.display = "none";
 };
